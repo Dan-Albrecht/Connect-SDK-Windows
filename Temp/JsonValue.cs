7bf3b23cd4a2c51fcdbe8ba3fdcc2fa35c8e0cf3
Dan Albrecht <Dan-Albrecht@users.noreply.github.com>
2019-12-15 16:08:17 -0800

    Add temp helper

---
+++ Temp/JsonValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace ConnectSdk
+{
+    class JsonValue
+    {
+        internal static string CreateStringValue(string iD)
+        {
+            return iD;
+        }
+
+        internal static double CreateNumberValue(double lastConnected)
+        {
+            return lastConnected;
+        }
+    }
+}
